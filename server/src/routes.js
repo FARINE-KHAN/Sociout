ace5393e123d44a472683bfa82c90966cfeeedbc
Muhaz <muhazvla313@gmail.com>
2023-02-20 22:35:28 +0530

    updated new routes

--- server/src/routes.js
+++ server/src/routes.js
@@ -4,6 +4,8 @@ const router = express.Router()
 const userC=require("./controllers/userController")
 
 router.post("/register",userC.register)
+router.post("/login",userC.login)
+router.post("/user/logout", userC.logout);
 router.put("/edit/:id",userC.editUser)
 router.delete("/delete/:id",userC.deleteUser)
 router.get("/fetch/:id",userC.fetchUser)
