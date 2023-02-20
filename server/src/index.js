997826097a93d5425b885355d0e02947558ff918
Muhaz <muhazvla313@gmail.com>
2023-02-20 22:35:11 +0530

    Added cors

--- server/src/index.js
+++ server/src/index.js
@@ -4,10 +4,12 @@ require("./models/db")
 require("dotenv").config();
 const route =require("./routes")
 const multer = require("multer")
+const cookieParser = require("cookie-parser")
 app.use( multer().any())
 
 
 app.use(express.json())
+app.use(cookieParser())
 app.use("/",route)
 
 
