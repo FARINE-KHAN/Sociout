09df597d9e95b5a06d890ee21ddd7b8b6ba848ef
mehwish <afreenmehwish2@gmail.com>
2023-03-01 23:26:39 +0530

    profile jsx

--- client/src/pages/user/Profile.jsx
+++ client/src/pages/user/Profile.jsx
@@ -1,10 +1,63 @@
-import React from 'react'
-import Maintenance from '../../components/Maintenance'
+import React, { useContext, useState } from 'react'
+import { AuthContext } from "../../Context/AuthContext";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Profile = () => {
+  const [values, setValues] = useState({
+    fullName: '',
+    email: '',
+    phone: '',
+    password: ''
+  })
+
+  const toastOptions = {
+    position: "bottom-right",
+    autoClose: 8000,
+    pauseOnHover: true,
+    draggable: true,
+    theme: "dark",
+  };
+
+  const { currentUser } = useContext(AuthContext)
+  let id = currentUser.User.id
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      await axios.put(`http://localhost:9000/edit/${id}`, values);
+    } catch (err) {
+      toast.error(err.response.data, toastOptions);
+    }
+  };
+
+  const handleDelete = async (event) => {
+    event.preventDefault();
+    try {
+      await axios.delete(`http://localhost:9000/delete/${id}`, values);
+    } catch (err) {
+      toast.error(err.response.data, toastOptions);
+    }
+  };
+
+  const handleChange = (event) => {
+    setValues({ ...values, [event.target.name]: event.target.value });
+  };
+
   return (
     <div>
-      <Maintenance/>
+      <div className='profile'><h1> PROFILE </h1>
+        <form>
+          <input placeholder='Full Name' name='fullName' onChange={handleChange} />
+          <input placeholder='Email' name='email' onChange={handleChange} />
+          <input placeholder='Phone' name='phone' onChange={handleChange} />
+          <input placeholder='password' name='password' onChange={handleChange} />
+
+          <button onClick={handleSubmit}> Update </button>
+          <button onClick={handleDelete} > Delete Account </button>
+        </form>
+
+      </div>
     </div>
   )
 }
